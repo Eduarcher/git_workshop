fixed wrong value

test.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-a = np.sum([1, 2, 443])
+a = np.sum([1, 2, 44])
 b = np.array([5, 4, 3])
 
 print(a*b)